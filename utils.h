4a056fee2127e5dc4558258d21711a21a635991d
Arthur <120437232+arthurFeijo@users.noreply.github.com>
2025-07-17 11:49:24 -0300

    Update utils.h

--- utils.h
+++ utils.h
@@ -2,6 +2,8 @@
 #define UTILS_H
 
 #include <vector>
+#include <map>
+#include <string>
 #include "corretor.h"
 #include "cliente.h"
 #include "imovel.h"
@@ -26,5 +28,25 @@ double haversine(double lat1, double lon1, double lat2, double lon2);
 void lerEntrada(std::vector<Corretor>& corretores, std::vector<Cliente>& clientes, std::vector<Imovel>& imoveis);
 
 void roundRobin (vector<Imovel>& imoveis, const vector<Corretor>& corretores);
+
+// Agendamento
+class Imovel;
+class Corretor;
+
+struct Agendamento {
+    int imovelId;
+    int corretorId;
+    std::string horario;
+};
+
+std::string minutosParaHorario(int minutos);
+
+std::vector<Agendamento> agendarVisitasPorVizinhoMaisProximo(
+    std::vector<Imovel>& imoveis,
+    const std::vector<Corretor>& corretores,
+    const std::map<int, int>& imovelParaCorretor
+);
+
+
 
 #endif // UTILS_H
